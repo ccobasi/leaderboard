1d600f9be0e6cf7d2718af6e83e2729edb1054b6
[Chukwuma Obasi] <ccobasi8@gmail.com>
2021-08-04 18:50:17 +0100

    Add createGame

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './style.css';
+
+const leaderboard = document.getElementById('leaderboard');
+
+const createGame = (name) => {
+    leaderboard.post('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/mchCFxyMLOw3tEZP2NKf/scores/',
+    name).then((response) => {
+        const list = document.createElement('ul');
+        list.classList.add('list-group');
+        list.innerHTML = `${response.result}`;
+        leaderboard.appendChild(list);
+    })
+}
