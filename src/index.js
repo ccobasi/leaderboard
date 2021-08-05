9ca644366d065530f2ad8a50d107cf5259604ee7
[Chukwuma Obasi] <ccobasi8@gmail.com>
2021-08-05 08:03:02 +0100

    Fix linters error

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
+/* eslint-disable no-return-assign */
 import './style.css';
-
 
 const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/mchCFxyMLOw3tEZP2NKf/scores/';
 const leaderboard = document.getElementById('leaderboard');
@@ -9,39 +9,34 @@ const score = document.getElementById('score');
 const submit = document.getElementById('add');
 
 const createGame = (name) => {
-    leaderboard.innerHTML = '';
-    name.result.map((e, index) => {
-    leaderboard.innerHTML += `<li class="list-group-item">${name.result[index].user} ${name.result[index].score}</li>`;
-  });
+  leaderboard.innerHTML = '';
+  name.result.map((e, index) => leaderboard.innerHTML += `<li class="list-group-item">${name.result[index].user} ${name.result[index].score}</li>`);
 
-  leaderboard.appendChild(name); 
-  
-}
+  leaderboard.appendChild(name);
+};
 
 const callApi = async () => {
-    const getData = await fetch(url);
-    const resource = await getData.json();
-    createGame(resource);
-    return resource;
-}
+  const getData = await fetch(url);
+  const resource = await getData.json();
+  createGame(resource);
+  return resource;
+};
 
 callApi();
 
 const addScore = () => {
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json;',
-      },
-      body: JSON.stringify({
-        user: name.value,
-        score: score.value,
-      }),
-    })
-      .then((response) => response.json())
-  }
-
-
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json;',
+    },
+    body: JSON.stringify({
+      user: name.value,
+      score: score.value,
+    }),
+  })
+    .then((response) => response.json());
+};
 
 refresh.addEventListener('click', callApi);
 submit.addEventListener('click', addScore);
